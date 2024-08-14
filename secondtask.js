27e25330b6460c310959306043a3e8224122c4e4
ahsannadeem52 <ahsannadeemsnq@gmail.com>
2024-08-14 06:40:52 +0500

    Task

--- secondtask.js
+++ secondtask.js
@@ -26,3 +26,59 @@ pre.addEventListener('click', () => {
     }
     upDateslider();
 });
+var imageslide = document.getElementById('trend');
+var pre1 = document.getElementById('pre1');
+var next1 = document.getElementById('Next1'); 
+var totalimage = 2;
+var currentitem = 0;
+function upDateSlider(){
+    imageslide.style.transform = `translateX(-${currentitem * 100}%)`;
+}
+
+next1.addEventListener('click', () => {
+    console.log("Next button clicked");
+    if (currentitem === totalimage - 1) {
+        currentitem = 0;
+    } else {
+        currentitem++;
+    }
+    upDateSlider();
+});
+
+pre1.addEventListener('click', () => {
+    console.log("Previous button clicked");
+    if (currentitem === 0) {
+        currentitem = totalimage - 1;
+    } else {
+        currentitem--;
+    }
+    upDateSlider();
+});
+var imageSlide = document.getElementById('deals');
+var pre2 = document.getElementById('pre2');
+var next2 = document.getElementById('Next2'); 
+var totalimage = 2;
+var currentitem = 0;
+function updateSlider(){
+    imageSlide.style.transform = `translateX(-${currentitem * 100}%)`;
+}
+
+next2.addEventListener('click', () => {
+    console.log("Next button clicked");
+    if (currentitem === totalimage - 1) {
+        currentitem = 0;
+    } else {
+        currentitem++;
+    }
+    updateSlider();
+});
+
+pre2.addEventListener('click', () => {
+    console.log("Previous button clicked");
+    if (currentitem === 0) {
+        currentitem = totalimage - 1;
+    } else {
+        currentitem--;
+    }
+    updateSlider();
+});
