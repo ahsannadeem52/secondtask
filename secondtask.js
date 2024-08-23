b6d5de7c40d3278f1547fc37b0843815dcfd07b7
ahsannadeem52 <ahsannadeemsnq@gmail.com>
2024-08-23 21:57:12 +0500

    Task

--- secondtask.js
+++ secondtask.js
@@ -1,7 +1,7 @@
 var slides = document.getElementById('discover');
 var pre = document.getElementById('pre');
 var next = document.getElementById('Next'); 
-var totalimage = 2;
+var totalimage = 13 ;
 var currentitem = 0;
 function upDateslider(){
     slides.style.transform = `translateX(-${currentitem * 100}%)`;
@@ -9,7 +9,7 @@ function upDateslider(){
 
 next.addEventListener('click', () => {
     console.log("Next button clicked");
-    if (currentitem === totalimage - 1) {
+    if (currentitem === totalimage) {
         currentitem = 0;
     } else {
         currentitem++;
@@ -26,36 +26,9 @@ pre.addEventListener('click', () => {
     }
     upDateslider();
 });
-var imageslide = document.getElementById('trend');
-var pre1 = document.getElementById('pre1');
-var next1 = document.getElementById('Next1'); 
-var totalimage = 2;
-var currentitem = 0;
-function upDateSlider(){
-    imageslide.style.transform = `translateX(-${currentitem * 100}%)`;
-}
 
-next1.addEventListener('click', () => {
-    console.log("Next button clicked");
-    if (currentitem === totalimage - 1) {
-        currentitem = 0;
-    } else {
-        currentitem++;
-    }
-    upDateSlider();
-});
-
-pre1.addEventListener('click', () => {
-    console.log("Previous button clicked");
-    if (currentitem === 0) {
-        currentitem = totalimage - 1;
-    } else {
-        currentitem--;
-    }
-    upDateSlider();
-});
 var imageSlide = document.getElementById('deals');
-var pre2 = document.getElementById('pre2');
+var Pre2 = document.getElementById('pre2');
 var next2 = document.getElementById('Next2'); 
 var totalimage = 2;
 var currentitem = 0;
@@ -73,7 +46,7 @@ next2.addEventListener('click', () => {
     updateSlider();
 });
 
-pre2.addEventListener('click', () => {
+Pre2.addEventListener('click', () => {
     console.log("Previous button clicked");
     if (currentitem === 0) {
         currentitem = totalimage - 1;
